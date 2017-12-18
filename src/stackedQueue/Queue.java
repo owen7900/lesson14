b94c960f712c0c710bf2ddf8dc7a102e00e9c083
owen7900 <owen7900@scdsb.on.ca>
2017-12-18 10:10:05 -0500

    made

---
+++ src/stackedQueue/Queue.java
@@ -0,0 +1,8 @@
+package stackedQueue;
+
+public interface Queue{
+	public Object peekFront();
+	public Object dequeue();
+	public void enqueue(Object o);
+	public int size();
+}
---
+++ src/stackedQueue/Stack.java
@@ -0,0 +1,8 @@
+package stackedQueue;
+
+public interface Stack{
+	public Object peekTop();
+	public Object pop();
+	public void push(Object o);
+	public int size();
+}
